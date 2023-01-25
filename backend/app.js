079c4e5c9a0f85e96297cc723941790c66402e4a
Umesh Kumar <umesh98_soe@jnu.ac.in>
2023-01-25 22:19:05 +0530

    updating build

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,6 @@ if (env !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -19,12 +18,10 @@ const user = require("./userRoute");
 
 app.use("/api/v1", user);
 
-if (env === "PRODUCTION") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 // middleware for errors
 app.use(errorMiddleware);
